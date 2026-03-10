19bda48b1bf5202bf5432ef39ca077dd67aee935
mohamed <mohamedlfarh7@gmail.com>
2026-03-10 09:59:52 +0000

    create les models de projet

---
+++ app/Models/HabitLog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class HabitLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'habit_id',
+        'completed_at',
+        'note',
+    ];
+
+    public function habit(): BelongsTo
+    {
+        return $this->belongsTo(Habit::class);
+    }
+}
